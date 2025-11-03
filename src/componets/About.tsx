7a2a31e134d6738283300260fdd621f18b524438
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-11-03 10:19:17 +0100

    Implement Motivation component and update NavBar and App to include it

--- src/componets/About.tsx
+++ src/componets/About.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion"; // Trigger animations when section is visible
-import { Smartphone, Zap, Code2 } from "lucide-react";
+import { Smartphone, Zap, Code2, Heart, Target } from "lucide-react";
 
 const About = () => {
   // Create a ref to track when this section is in viewport
@@ -29,6 +29,25 @@ const About = () => {
       title: "Performance Focused",
       description:
         "Optimizing for speed, accessibility, and SEO to deliver exceptional user experiences.",
+    },
+
+    {
+      icon: Target,
+      title: "Passion for Excellence",
+      description:
+        "Dedicated to delivering high-quality code and exceptional user experiences.",
+    },
+    {
+      icon: Zap,
+      title: "Continuous Learning",
+      description:
+        "Always staying updated with the latest technologies and best practices.",
+    },
+    {
+      icon: Heart,
+      title: "Team Collaboration",
+      description:
+        "Thriving in collaborative environments and contributing to team success.",
     },
   ];
 
